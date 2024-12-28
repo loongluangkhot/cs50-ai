d645c83b0f5b2179b968f2270edda2a4233d35ad
loongluangkhot <loongluangkhot@gmail.com>
2024-12-28 15:21:40 +0800

    complete hereditary

--- uncertainty/heredity/heredity.py
+++ uncertainty/heredity/heredity.py
@@ -139,7 +139,64 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         * everyone in set `have_trait` has the trait, and
         * everyone not in set` have_trait` does not have the trait.
     """
-    raise NotImplementedError
+    prob_map = {}
+    for person_name in people:
+        person = people[person_name]
+        mother = person["mother"]
+        father = person["father"]
+        has_parents = mother is not None and father is not None
+        has_trait = person_name in have_trait
+        num_of_genes = get_num_of_genes(person_name, one_gene, two_genes)
+        if has_parents:
+            mother_num_of_genes = get_num_of_genes(mother, one_gene, two_genes)
+            father_num_of_genes = get_num_of_genes(father, one_gene, two_genes)
+            prob_map[person_name] = get_prob_for_person_with_parents(
+                num_of_genes, has_trait, mother_num_of_genes, father_num_of_genes)
+        else:
+            prob_map[person_name] = get_prob_for_person_with_no_parents(num_of_genes, has_trait)
+    joint_probability = 1
+    for person_name in prob_map:
+        joint_probability *= prob_map[person_name]
+    return joint_probability
+
+
+def get_num_of_genes(person_name, one_gene, two_genes):
+    if person_name in one_gene:
+        return 1
+    elif person_name in two_genes:
+        return 2
+    else:
+        return 0
+
+
+def get_prob_for_person_with_no_parents(num_of_genes: int, has_trait: bool):
+    return PROBS["gene"][num_of_genes] * PROBS["trait"][num_of_genes][has_trait]
+
+
+def get_prob_for_person_with_parents(num_of_genes: int, has_trait: bool, mother_num_of_genes: int, father_num_of_genes: int):
+    prob_of_getting_from_father = get_prob_of_person_getting_gene_from_parent(father_num_of_genes)
+    prob_of_getting_from_mother = get_prob_of_person_getting_gene_from_parent(mother_num_of_genes)
+    prob_of_not_getting_from_father = 1 - prob_of_getting_from_father
+    prob_of_not_getting_from_mother = 1 - prob_of_getting_from_mother
+    
+    if num_of_genes == 0:
+        prob_of_person_with_num_of_genes = prob_of_not_getting_from_father * prob_of_not_getting_from_mother
+    elif num_of_genes == 1:
+        prob_of_person_with_num_of_genes = prob_of_getting_from_father * prob_of_not_getting_from_mother + \
+            prob_of_not_getting_from_father * prob_of_getting_from_mother
+    else:
+        prob_of_person_with_num_of_genes = prob_of_getting_from_father * prob_of_getting_from_mother
+
+    return prob_of_person_with_num_of_genes * PROBS["trait"][num_of_genes][has_trait]
+
+
+def get_prob_of_person_getting_gene_from_parent(parent_num_of_genes: int):
+    if parent_num_of_genes == 0:
+        return PROBS["mutation"]
+    elif parent_num_of_genes == 1:
+        return 0.5
+    else:
+        return 1 - PROBS["mutation"]
 
 
 def update(probabilities, one_gene, two_genes, have_trait, p):
@@ -149,7 +206,11 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
     Which value for each distribution is updated depends on whether
     the person is in `have_gene` and `have_trait`, respectively.
     """
-    raise NotImplementedError
+    for person_name in probabilities:
+        num_of_genes = get_num_of_genes(person_name, one_gene, two_genes)
+        has_trait = person_name in have_trait
+        probabilities[person_name]["gene"][num_of_genes] += p
+        probabilities[person_name]["trait"][has_trait] += p
 
 
 def normalize(probabilities):
@@ -157,7 +218,15 @@ def normalize(probabilities):
     Update `probabilities` such that each probability distribution
     is normalized (i.e., sums to 1, with relative proportions the same).
     """
-    raise NotImplementedError
+    for person_name in probabilities:
+        for field in probabilities[person_name]:
+            normalize_distribution(probabilities[person_name][field])
+
+
+def normalize_distribution(distribution):
+    total = sum(distribution.values())
+    for value in distribution:
+        distribution[value] /= total
 
 
 if __name__ == "__main__":
