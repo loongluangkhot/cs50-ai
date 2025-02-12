18d0952f3f2371cc2bc9d0762b0526658f5b6b81
loongluangkhot <loongluangkhot@gmail.com>
2025-02-12 21:05:50 +0800

    wip parser (left with last test)

--- language/parser/parser.py
+++ language/parser/parser.py
@@ -1,5 +1,9 @@
 import nltk
 import sys
+import re
+import os
+
+nltk.download('punkt_tab', quiet=True)
 
 TERMINALS = """
 Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
@@ -15,7 +19,12 @@ V -> "smiled" | "tell" | "were"
 """
 
 NONTERMINALS = """
-S -> N V
+S -> NP VP | S Conj S | S Conj VP
+VP -> V | VP NP | Adv VP | VP Adv
+NP -> FQN | P NP | NP Conj NP
+CN -> N | N CN
+QN -> CN | Adj QN 
+FQN -> QN | Det QN | FQN Adv
 """
 
 grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
@@ -62,7 +71,14 @@ def preprocess(sentence):
     and removing any word that does not contain at least one alphabetic
     character.
     """
-    raise NotImplementedError
+    tokens = nltk.tokenize.word_tokenize(sentence.lower())
+    pattern = r"[a-z]+"
+
+    def filter_func(i):
+        return re.match(pattern, i) is not None
+    
+    filtered_tokens = list(filter(filter_func, tokens))
+    return filtered_tokens
 
 
 def np_chunk(tree):
@@ -72,7 +88,37 @@ def np_chunk(tree):
     whose label is "NP" that does not itself contain any other
     noun phrases as subtrees.
     """
-    raise NotImplementedError
+    def is_np_tree(t):
+        return t.label() == "NP"
+
+    def is_np_chunk_tree(t): 
+        return is_np_tree(t) and list(t.subtrees(is_np_tree))
+    
+    return list(tree.subtrees(is_np_chunk_tree))
+
+
+def test(s):
+    print(s)
+    s = preprocess(s)
+    trees = list(parser.parse(s))
+    if not trees:
+        raise Exception("Could not parse sentence.")
+    for tree in trees:
+        tree.pretty_print()
+
+        print("Noun Phrase Chunks")
+        for np in np_chunk(tree):
+            print(" ".join(np.flatten()))
+
+
+def test_all_files():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    for i in range(10):
+        file_path = os.path.join(current_dir, "sentences", f"{i+1}.txt")
+        print(file_path)
+        with open(file_path, "r") as f:
+            s = f.read()
+        test(s)
 
 
 if __name__ == "__main__":
